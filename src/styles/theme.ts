30535acb559af3a9610a9a19e5c8c356614354e1
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2022-08-29 16:05:51 -0300

    feat: 🎸 font changes

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -6,6 +6,7 @@ export const theme = extendTheme({
             body: {
                 bg: 'gray.900',
                 color: 'gray.50',
+                fontSize: '1rem',
             }
         }
     }
