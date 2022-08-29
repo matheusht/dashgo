4fa04a634ead908b3a676ea599d125d90a5b6ecf
matheusht <dev.matheustheodoro@gmail.com>
2022-08-29 16:18:28 -0300

    feat: 🎸 ReactDOM import to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ReactDOM } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
 import { theme } from '../styles/theme';
