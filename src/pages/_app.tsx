56d6be918900b532918a7097b8e70e70158db68a
matheusht <dev.matheustheodoro@gmail.com>
2022-08-29 21:30:18 -0300

    font added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { ReactDOM } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
 import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
-
+import { Flex } from '@chakra-ui/react'
 
 export default function Home() {
   return (
-    <h1>Dashgo</h1>
+    <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center">
+      <h1>coco</h1>
+    </Flex>
   )
 }
