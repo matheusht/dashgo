04afd870eefa01d4940af3d26dc7d54cbdd05304
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2022-08-31 15:29:45 -0300

    Flexbox component styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,59 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, Input, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react'
 
 export default function Home() {
   return (
-    <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center">
-      <h1>coco</h1>
+    <Flex w="100vw"
+      h="100vh"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius="8"
+        flexDirection="column"
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="E-mail">Email</FormLabel>
+
+            <Input
+              name="email"
+              id="email"
+              type="email"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: "gray.900",
+              }}
+              size="lg"
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="Password">Password</FormLabel>
+
+            <Input
+              name="password"
+              type="password"
+              id="password"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: "gray.900",
+              }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">Entrar</Button>
+      </Flex>
     </Flex>
   )
 }
