ed1e31a2fbd055ddcb1977653bd3c966f1d7df0b
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2022-09-01 20:29:08 -0300

    Header finish + sidebar features

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
-import { Flex, Text, Input } from '@chakra-ui/react'
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react'
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header() {
     return (
@@ -44,7 +45,40 @@ export function Header() {
                     placeholder="Buscar na plataforma"
                     _placeholder={{ color: 'gray.400' }}
                 />
+
+                <Icon as={RiSearchLine} fontSize="20" />
             </Flex>
+
+            <Flex
+                align="center"
+                ml="auto"
+            >
+                <HStack
+                    spacing="4"
+                    mx="8"
+                    pr="8"
+                    py="1"
+                    color="gray.300"
+                    borderRightWidth={1}
+                    borderColor="gray.700"
+                >
+                    <Icon as={RiNotificationLine} fontSize="20" />
+                    <Icon as={RiUserAddLine} fontSize="20" />
+                </HStack>
+
+                <Flex
+                    align="center"
+                >
+                    <Box mr="4" textAlign="right">
+                        <Text>Caio</Text>
+                        <Text color="gray.300" fontSize="small">
+                            dev.caiotheodoro@gmail.com
+                        </Text>
+                    </Box>
+                </Flex>
+            </Flex>
+
+            <Avatar size="md" name='RIAN' src="https://github.com/caiotheodoro.png" />
         </Flex >
     );
 }
