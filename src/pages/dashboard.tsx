ea784cb7d7313c4a0ff40ff62f669e5742490d51
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2022-09-03 14:08:18 -0300

    apexchart stylization changes

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,14 +1,62 @@
-import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
+import dynamic from 'next/dynamic'
 import { Sidebar } from "../components/Form/Sidebar";
 import { Header } from "../components/Header";
-import Chart from 'react-apexcharts';
+
+const Chart = dynamic(() => import('react-apexcharts'), {
+    ssr: false,
+})
 
 const options = {
-
+    chart: {
+        toolbar: {
+            show: false,
+        },
+        zoom: {
+            enabled: false,
+        },
+        foreColor: theme.colors.gray[500],
+    },
+    grid: {
+        show: false,
+    },
+    dataLabels: {
+        enabled: false,
+    },
+    tooltip: {
+        enabled: false,
+    },
+    xaxis: {
+        type: 'datetime',
+        axisBorder: {
+            color: theme.colors.gray[600],
+        },
+        axisTicks: {
+            color: theme.colors.gray[600],
+        },
+        categories: [
+            '2022-09-03T00:00:00.000Z',
+            '2022-09-04T00:00:00.000Z',
+            '2022-09-05T00:00:00.000Z',
+            '2022-09-06T00:00:00.000Z',
+            '2022-09-07T00:00:00.000Z',
+            '2022-09-08T00:00:00.000Z',
+            '2022-09-09T00:00:00.000Z',
+        ],
+    },
+    fill: {
+        opacity: 0.3,
+        type: 'gradient',
+        gradient: {
+            shade: 'dark',
+            opactiyFrom: 0.7,
+            opactiyTo: 0.3,
+        }
+    },
 };
 
 const series = [
-    { name: 'series-1', data: [18, 41, 35, 51, 49, 62, 69] },
+    { name: 'series-1', data: [18, 80, 35, 69, 49, 62, 89] },
 ]
 
 export default function Dashboard() {
@@ -36,6 +84,7 @@ export default function Dashboard() {
                     // pb="4"
                     >
                         <Text fontSize="lg" mb="4">Taxa de abertura</Text>
+                        <Chart options={options} series={series} type="area" height={160} />
                     </Box>
 
                 </SimpleGrid>
